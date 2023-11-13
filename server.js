3b8d8cd6e666444e93e67f61350ece59b9e18f9f
DARIOO33 <darioanwar3@gmail.com>
2023-11-13 18:42:57 +0100

    first commit

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const cors = require("cors");
 
 const app = express()
 
-app.use(cors({
-    origin: ["http://localhost:1000",
-        "https://mern-workouts-app.onrender.com"]
-}));
+app.use(cors());
 app.use(express.json())
 
 
